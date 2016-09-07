ee01a6b86d808b9d7200951d77228851380a86e0
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-09-07 21:25:59 +0000

    Add the cthread.c to the project with the ccreate function.

---
+++ src/cthread.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include "../include/cthread.h"
+#include "../include/cdata.h"
+#include <stdio.h>
+
+int inicializado = 0;
+
+int ccreate (void* (*start)(void*), void *arg){
+	//Inicializar filas e TCB da main
+
+	}
+
+};
