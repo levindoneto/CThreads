a688ca8c00824c2ddb11328b7ffe7f4d7dee5c14
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2016-09-07 21:41:48 +0000

    Add the queues and create thread.

--- src/cthread.c
+++ src/cthread.c
@@ -2,15 +2,25 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
+#include "../include/support.h"
 #include "../include/cthread.h"
 #include "../include/cdata.h"
 #include <stdio.h>
 
-int inicializado = 0;
+int init = 0;
 
 int ccreate (void* (*start)(void*), void *arg){
-	//Inicializar filas e TCB da main
+	//__init__ queue and main's TCB
+	if(init != 1){ // Thread has not initialized yet
+		able_queue = (PFILA2) malloc(sizeof(PFILA2));
+		block_join = (PFILA2) malloc(sizeof(PFILA2));
+		block = (PFILA2) malloc(sizeof(PFILA2));
+		
+		CreateFila2(able_queue);
+		CreateFila2(block_join);
+		CreateFila2(block);
 
+		thread_main = TCB_create_main();
+		init = 1;    // __init__ thread
 	}
-
-};
+	}
